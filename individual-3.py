7702f839c76885462294a823f976ccdef13ddffc
RomanGorchakov <144220160+RomanGorchakov@users.noreply.github.com>
2023-12-13 12:14:07 +0300

    Update individual-3.py

--- individual-3.py
+++ individual-3.py
@@ -1,7 +1,8 @@
-n = 10
+if __name__ == '__main__':
+    n = 10
 
-for i in range(1, 100):
-    n += n / 10
-    if n > 15:
-        print(i)
-        break
+    for i in range(1, 100):
+        n += n / 10
+        if n > 15:
+            print(i)
+            break
