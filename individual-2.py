80be4b800a4ed40294677e51522b092cfafb0da6
RomanGorchakov <144220160+RomanGorchakov@users.noreply.github.com>
2023-12-13 12:13:50 +0300

    Update individual-2.py

--- individual-2.py
+++ individual-2.py
@@ -1,17 +1,20 @@
-a, b, c = list(map(float, input("Введите три числа: ").split()))
-up = []
-up.append(a)
-up.append(b)
-up.append(c)
+if __name__ == '__main__':
+    a, b, c = list(map(float, input("Введите три числа: ").split()))
+    
+    up = []
+    up.append(a)
+    up.append(b)
+    up.append(c)
 
-i = 0
-while i < 2:
-    j = 0
-    while j < 2 - i:
-        if up[j] > up[j+1]:
-            up[j], up[j+1] = up[j+1], up[j]
-        j += 1
-    i += 1
-print(up)
-up.reverse()
-print(up)
+    i = 0
+    while i < 2:
+        j = 0
+        while j < 2 - i:
+            if up[j] > up[j+1]:
+                up[j], up[j+1] = up[j+1], up[j]
+            j += 1
+        i += 1
+    print(up)
+
+    up.reverse()
+    print(up)
